b0334623b64cf8e77327c3822e78f3fdb4c3f6b1
Priya Bhasin <prattan@usc.edu>
2020-05-17 10:38:57 -0700

    Add new files

---
+++ family_data.js
@@ -0,0 +1,127 @@
+family_data = {
+  "name": "👳Bhai Teja Singh 💕 👩 Nihal Kaur",
+  "children": [{
+    "name": "👳‍♂️Harbhajan Singh 💕 👩Jaiwanti",
+    "children": [{
+      "name": "👳‍♂️Bhupinder Singh 💕 👩Narinder Kaur",
+      "children": [{
+          "name": "👳‍♂️Arvind Singh Bhasin 💕 👩Gulroop Kaur",
+          "children": [{
+            "name": "👳‍♂️Tanmay Singh Bhasin"
+          }, {
+            "name": "👩Ishret Kaur Bhasin"
+          }]
+        },
+
+        {
+          "name": "👳‍♂️Bimaljit Singh Bhasin 💕 Abha Bhasin",
+          "children": [{
+            "name": "👳‍♂️Keerat Singh"
+          }, {
+            "name": "👩Simar Kaur"
+          }]
+        }
+      ]
+    }, {
+      "name": "👩Amarjit Kaur 💕 👳‍♂️Gurbaksh Singh Bhasin",
+      "children": [{
+        "name": "👳‍♂️Jasmit Singh Bhasin 💕 👩Poonam Kaur",
+        "children": [{
+          "name": "👳‍♂️Pavit Singh"
+        }]
+      }, {
+        "name": "👩Harmit Kaur 💕 👳‍♂️Gurmeet Singh",
+        "children": [{
+          "name": "👳‍♂️Japnit Singh"
+        }, {
+          "name": "👳‍♂️Gurtej Singh"
+        }]
+      }]
+    }, {
+      "name": "👳‍♂️Joginder Bir Singh Hazooria 💕 👩Gursharan Kaur",
+      "children": [{
+        "name": "👩Ishmeet Kaur Bhasin 💕 👳‍♂️Gurvinder Singh",
+        "children": [{
+          "name": "👩Harbani Kaur"
+        }, {
+          "name": "👩Geet Kaur"
+        }]
+      }, {
+        "name": "👩Jasneet Kaur"
+      }, {
+        "name": "👳‍♂️Chirangad Singh Bhasin 💕 Priya"
+      }]
+
+    }, {
+      "name": "👳‍♂️Jagjit Singh Hazooria 💕 👩Ravinder Kaur",
+      "children": [{
+        "name": "👳‍♂️Sundeep Singh Bhasin 💕 to 👳‍♂️Rinki Singh",
+        "children": [{
+          "name": "Raina"
+        }, {
+          "name": "Sanya"
+        }]
+      }, {
+        "name": "👩Khushpreet Kaur 💕 Salil Sethi"
+      }, ]
+    }, {
+      "name": "👳‍♂️Kawalijit Singh Bhasin 💕 👩Praveen Kaur",
+      "children": [{
+        "name": "👳‍♂️Anhad Singh Bhasin 💕 👩Bavnit Kaur"
+      }, {
+        "name": "Priya Bhasin 💕 Souvik Roy"
+      }]
+
+    }]
+  }, {
+    "name": "👳‍♂️Trilochan Singh 💕 👩 Jaswant kaur",
+    "children": [{
+      "name": "👳‍♂️Kulwant Singh Hazooria 💕 👩 Manohar Chadha",
+      "children": [{
+        "name": "👩 Kawal Sahni 💕 Sarabjit Sahni",
+        "children": [{
+          "name": "👳‍♂️Arshdeep Sahni 💕 👩 Gunweet Manchanda",
+          "children": [{
+            "name": "👳‍♂️Jahhanbir Sahni"
+          }]
+        }, {
+          "name": "👳‍♂️Harjot Sahni",
+        }]
+      }, {
+        "name": "👩 Amrita Kataria 💕 👳‍♂️Jaideep Kataria",
+        "children": [{"name": "Gurpriya Luthra 💕 👳‍♂️ Sharan Luthra",
+                      "children": [{"name": "Karam Luthra"}], 
+          },{
+             "name": "Sahibjit Kataria"
+          }]
+      }, {
+        "name": "👩 Seema Kathuria 💕 👳‍♂️Gurbachan",
+        "children": [{
+          "name": "Tarunveer Kathuria"
+        }, {
+          "name": "Garima Kathuria"
+        }]
+      }, {
+        "name": "👩 Danita Sawhney 💕 👳‍♂️Jasminder Singh Sawhney",
+        "children": [{"name":"👩Rabani Sawhney"},{"name":"👩 Meher Sawhney"}]
+      }, {
+        "name": "👩 Geetika Anand 💕 👳‍♂️ Sanjeev Singh Anand",
+        "children": [{"name":"👩 Revaa Anand"},{"name":"👳‍♂️Jaiveer Anand"},{"name":"Kabir Anand"}]
+      }]
+    }, {
+      "name": "Nirmal Bedi 💕 Sardar Singh Bedi",
+      "children":[{"name":"👩 Reena Kohli 💕 👳‍♂️Surjit Singh Kohli","children":
+                    [{"name":"Gurpreet Kohli"},{"name":"Samara Kohli"}]},
+                  {"name":"👳‍♂️ Binder Bedi 💕 👳‍♂️Dolly Bedi","children":[{"name":"Tejkaran Bedi"}]}]
+    }, {
+      "name": "👳‍♂️Harcharan Singh Hazooria 💕 👩Teena Hazooria",
+      "children":[{"name":"👩 Anushka Hazooria","name":"👳‍♂️Harsh Hazooria"}]
+    }]
+  }, {
+    "name": "👩 Laaj Kaur"
+  }, {
+    "name": "👩 Anant Kaur 💕 Dhuna Singh"
+  }, {
+    "name": "👩Gyaan Kaur"
+  }]
+};
