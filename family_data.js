9f198052807ac0d3ea29aa6236b0662934eafa0f
“priya <rattanpriya_bhasin@apple.com>
2020-05-24 13:44:54 -0700

    added data

--- family_data.js
+++ family_data.js
@@ -118,7 +118,23 @@ var family_data = {
       "children":[{"name":"👩 Anushka Hazooria","name":"👳‍♂️Harsh Hazooria"}]
     }]
   }, {
-    "name": "👩 Laaj Kaur"
+    "name": "👩 Laaj Kaur","children":[
+                      {"name":"Gurcharan Singh Luthra married to Gajinder Kaur Luthra","children":[{"name":"Jagmohan Singh Luthra married to Neelam Luthra"},
+                                                                                                    {"name":"Jagdeep Singh Luthra married to Rekha Luthra"}]},
+                      {"name":"Bhagwant Kaur duggal married to Saroop Singh duggal","children":[{"name":"Satinder Kaur Sahi married to Gurcharan Singh Sahi"},
+                                                                                                {"name":"Swaranjit Singh duggal married to Harminder Kaur duggal"},
+                                                                                                {"name":"Harinder Singh duggal married to Surjit Kaur duggal"}]},
+                      {"name":"Davinder Singh Luthra married to Atamjit Kaur Luthra","children":[{"name":"Col.Harpreet Singh Luthra married to jasvenus kaur Luthra",
+                                                                                                "children":[{"name":"Chandni Luthra shahpuri married to Kavan Shahpuri",
+                                                                                                            "children":[{"name":"Amaira"},{"name":"Ziva"}]},{"name":"Mehak Ahluwalia married to Mick Ahluwalia","children":[{"name":"Aroon Ahluwalia"}]}]},
+                                                                                                {"name":"Gunmala Anand married to Manmohan Singh Anand",
+                                                                                                    "children":[{"name":"Harbani Anand married to Jaideep Singh Anand","children":[{"name":"Hunar Anand"},{"name":"Japnaa"}]},
+                                                                                                                {"name":"Sheena dhingra married to Harbir Singh dhingra","children":[{"name":"Amer"},{"name":"Reet"}]},
+                                                                                                                {"name":"Varun Anand married to Amrita Anand","children":[{"name":"Kunwar Singh Anand"}]}]},
+                                                                                                {"name":"Jaimala Kaur married to Mandeep Singh duggal",
+                                                                                                "children":[{"name":"Gunika Singh married to Hardeep Singh",
+                                                                                                            "children":[{"name":"Vaani"},{"name":"Meer"}]}]}]},
+                      {"name":"Surjit Singh Luthra married to Davinder Kaur Luthra","children":[{"name":"Leena Luthra shah married to Dr Syed Tasaddiq Ali Shah","children":[{"name":"Imran Syed Shah"},{"name":"Aliza Syed Shah"}]},{"name":"Puneet Luthra married to Anu Mehra Luthra","children":[{"name":"Aanya Luthra"}]},{"name":"Suneet Luthra married to Amrit chana Luthra","children":[{"name":"Tej Luthra"},{"name": "Kirin Luthra"}]}]}]
   }, {
     "name": "👩 Anant Kaur 💕 Dhuna Singh"
   }, {
@@ -142,11 +158,21 @@ var family_data = {
 
                   ]
                 },
-                {"name":"Gobind Kaur"},
-                {"name":"S Daljit Singh Sahni"},
-                {"name":"Harbans Kaur"},
-                {"name":"Ranjit Singh Sahni"},
-                {"name":"S Manjit Singh Sahni"
+                {"name":"Gobind Kaur","children":[{"name":"Ravinder Kaur Sethi married to Kulbir Singh Sethi","children":[{"name":"Aman Sethi married to Pavika","children":[{"name":"Ambreen Sethi"}]},{"name":"Ranbir Singh Sahni married to Gurinder Kaur"}]},
+                                                  {"name":"Ranbir Singh Sahni married to Gurinder Kaur","children":[{"name":"Kanchan Preet Kaur married to Gurpreet Singh","children":[{"name":"Prubjeev Sahni"},{"name":"Amaira"}]}]},
+                                                  {"name":"Dalbir Singh Sahni married to Harpreet Kaur","children":[{"name":"Simar Bir Singh"},{"name": "Gunisha"}]} 
+
+                ]},
+                {"name":"S Daljit Singh Sahni married to Pritam Kaur Sahni","children":[{"name":"Harneet Kaur Chadha married to Onderpal Singh Chadha","children":[{"name":"Anushka Chadha"}]},
+                                                                                {"name":"Ranbir Singh Sahni married to Natasha Sahni","children":[{"name":"Manav Bir Sahni"},{"name":"Guntash Sahni"}]}
+
+                                                                              ]},
+                {"name":"Harbans Kaur to Narinder Singh Sahni","children":[{"name":"Nancy Sahni married to Jake Metzler"},{"name":"Shaloo Sahni married to Mahmoud Arjomand","children":[{"name":"Banou"},{"name":"Jahan Arjomand"},{"name":"May"}]},{"name":"Lily Marlborough married to 11 th Duke of Marlborough"}]},
+                {"name":"Ranjit Singh Sahni married to Mohinder Kaur Sahni","children":[{"name":"Atam Bir Singh married Ruchika","children":[{"name":"Namrita Sahni"}]},
+                                                                                        {"name":"Harmeet Sethi married Anand Singh Sethi","children":[{"name":"Jaskirat"},{"name":"Harkirat"}]},
+                                                                                        {"name":"Babeet Singh Sahni married to Sukhpreet Singh","children":[{"name":"Bismanjit Singh"},{"name":"Arya Jit Singh"},{"name":"Mansa Gyan"}]},
+                                                                                        {"name":"Jasmine married to Ishpal Singh Sethi","children":[{"name":"Paraj"}]}]},
+                {"name":"S Manjit Singh Sahni married to Shireen Saher","children":[{"name":"Pheroza Gyan married to Eloy Prieto"},{"name":"Zarina Margaret"}]
                 }]
   }]
 };
